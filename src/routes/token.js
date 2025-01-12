366031e28b6377f87752c5bd62f0a02ef0af7268
fatihbaydar <fatihbaydar2004@gmail.com>
2025-01-12 22:52:49 +0300

    token

--- src/routes/token.js
+++ src/routes/token.js
@@ -1,7 +1,17 @@
 "use strict"
 
 const router = require('express').Router()
-/* ------------------------------------------------------- */
 
-/* ------------------------------------------------------- */
+const token = require("../controllers/token")
+
+router.route("/")
+.get(token.list)
+.post(token.create)
+
+router.route("/:id")
+.get(token.read)
+.post(token.update)
+.patch(token.update)
+.delete(token.delete)
+
 module.exports = router
