5536bb3386397ca5ed71320f0e2400fd54289149
fatihbaydar <fatihbaydar2004@gmail.com>
2025-01-13 16:53:47 +0300

    pizza

--- src/models/pizza.js
+++ src/models/pizza.js
@@ -1,4 +1,29 @@
 "use strict"
 
 const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+
+const PizzaSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            trim:true,
+            required:true,
+            unique:true
+        },
+        image: String,
+        price:{
+            type:Number,
+            required:true
+        },
+        toppingsIds:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Topping"
+            }
+        ]
+    }, {
+        collection:"pizzas", timestamps:true
+    }
+)
+
+module.exports = mongoose.model("Pizza", PizzaSchema)
--- src/routes/pizza.js
+++ src/routes/pizza.js
@@ -1,7 +1,16 @@
 "use strict"
 
 const router = require('express').Router()
-/* ------------------------------------------------------- */
+const pizza = require("../controllers/pizza")
 
-/* ------------------------------------------------------- */
+router.route("/")
+.get(pizza.list)
+.post(pizza.create)
+
+router.route("/:id")
+.get(pizza.read)
+.post(pizza.update)
+.patch(pizza.update)
+.delete(pizza.read)
+
 module.exports = router
