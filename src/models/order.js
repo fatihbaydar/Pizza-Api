46b50c9ee3ade6b1c829c8d01a3533b9ab30b023
fatihbaydar <fatihbaydar2004@gmail.com>
2025-01-13 16:00:10 +0300

    order

--- src/models/order.js
+++ src/models/order.js
@@ -1,4 +1,43 @@
 "use strict"
 
 const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+
+const OrderSchema = new mongoose.Schema ({
+    userId: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    pizzaId: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Pizza"
+    },
+    size:{
+        type:String,
+        trim:true,
+        required:true,
+        enum: ["Small", "Medium", "Large", "XLarge"]
+    },
+    quantity:{
+        type:Number,
+        default:1
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    amount:{
+        type:Number,
+        // create
+        default: function() {
+            return this.quantity * this.price
+        },
+        // update
+        transform: function() {
+            return this.quantity * this.price
+        }
+    }
+},{
+    collection:"orders",timestamps:true
+})  
+
+module.exports = mongoose.model("Order", OrderSchema)
