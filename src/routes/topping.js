93cfa4f07feebc566c5a5cb0e4a37b353378138e
fatihbaydar <fatihbaydar2004@gmail.com>
2025-01-13 17:55:48 +0300

    toppings

--- src/routes/topping.js
+++ src/routes/topping.js
@@ -1,7 +1,17 @@
 "use strict"
 
 const router = require('express').Router()
-/* ------------------------------------------------------- */
 
-/* ------------------------------------------------------- */
+const topping = require("../controllers/topping")
+
+router.route("/")
+.get(topping.list)
+.post(topping.create)
+
+router.route("/:id")
+.get(topping.read)
+.put(topping.update)
+.patch(topping.update)
+.delete(topping.delete)
+
 module.exports = router
