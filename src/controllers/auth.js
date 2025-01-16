6b1ed20539a3fb1a73e4a90798ae88fb378ee636
fatihbaydar <fatihbaydar2004@gmail.com>
2025-01-16 14:09:48 +0300

    jwt

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,6 +4,7 @@ const User = require("../models/user")
 const Token = require("../models/token")
 const { BadRequestError, NotFoundError, UnauthorizedError } = require("../errors/customError")
 const passwordEncrypt = require("../helpers/passwordEncrypt")
+const jwt = require("jsonwebtoken")
 
 module.exports = {
     login: async (req, res) => {
@@ -33,16 +34,44 @@ module.exports = {
         if (user.password !== passwordEncrypt(password))
             throw new UnauthorizedError("the password is incorrect")
 
+        /* SIMLPLE TOKEN */
         let tokenData = await Token.findOne({ userId: user._id })
 
         if (!tokenData) {
             const tokenKey = passwordEncrypt(user._id + Date.now())
             tokenData = await Token.create({ userId: user._id, token: tokenKey })
         }
+        /* /SIMLPLE TOKEN */
+
+        /* JWT */
+        // Access Token
+        const accessData = {
+            _id:user._id,
+            username:user.username,
+            email:user.email,
+            isActive:user.isActive,
+            isAdmin:user.isAdmin,
+        }
+        // convert to jwt
+        const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, {expiresIn:"30m"})
+
+        // Refresh Token
+        const refreshData = {
+            _id:user._id,
+            password:user.password
+        }
+        // convert to jwt
+        const refreshToken = jwt.sign(refreshData, process.env.REFRESH_KEY, {expiresIn:"3d"})
+
+        /* /JWT */
 
         res.status(200).send({
             error: false,
             token: tokenData.token,
+            bearer:{
+                access:accessToken,
+                refresh:refreshToken
+            },
             user,
         });
     },
@@ -57,8 +86,12 @@ module.exports = {
         const tokenKey = auth ? auth.split(" ") : null
         let deleted = null
 
-        if(tokenKey?.at(0) == "Token"){
-            deleted = await Token.deleteOne({token:tokenKey[1]})
+        if (tokenKey?.at(0) == "Token") {
+            deleted = await Token.deleteOne({ token: tokenKey[1] })
         }
+        res.send({
+            error: false,
+            message: "Token deleted. Logout was OK.",
+          });
     }
 }
