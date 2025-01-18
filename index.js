cb293ec0bbe3356abecb5d5c98963ff9959df855
fatihbaydar <fatihbaydar2004@gmail.com>
2025-01-18 15:32:54 +0300

    showing static files

--- index.js
+++ index.js
@@ -43,68 +43,12 @@ app.use(require('./src/middlewares/authentication'))
 // findSearchSortPage / res.getModelList:
 app.use(require('./src/middlewares/queryHandler'))
 
-//* EMAIL *NODEMAILER
-
-// const nodemailer = require('nodemailer')
-
-// nodemailer.createTestAccount().then((data => console.log(data)))
-
-// // Connection to MailServer/SMTP:
-// const transporter = nodemailer.createTransport({
-//     // SMTP
-//     host: "smtp.ethereal.email",
-//     port: 587,
-//     secure: false,
-//     auth: {
-//         user: "wsirtieron5opxi2@ethereal.email",
-//         pass: "u2CzpRa2S7wWC9TASq"
-//     }
-// })
-//   console.log(transporter)
-
-// // SendMail
-// transporter.sendMail({
-//     from: "wsirtieron5opxi2@ethereal.email",
-//     to: "fatihbaydar2004@gmail.com",
-//     subject: "Hello",
-//     text: "Hello. Welcome to our website...",
-//     html: "<h3>Hello. Welcome to our website...</h3><p>How are you?</p>"
-// }, function (error, success) {
-//     success ? console.log("SUCCESS:", success) : console.log("ERROR:", error)
-// })
-
-
-//* GoogleMail
-// Google -> AccountHome -> Security -> Two-Step-Verify -> App-Passwords
-// const transporter = nodemailer.createTransport({
-//     service:"gmail",
-//     auth:{
-//         user:"fatihbaydar2004@gmail.com",
-//         pass:"gorc mayj kltq wvpx"
-//     }
-// })
-
-//* YandexMail (yandex)
-// const transporter = nodemailer.createTransport({
-//     service: 'yandex',
-//     auth: {
-//         user: 'test@yandex.com',
-//         pass: '11' // your email-password
-//     }
-// })
-
-// transporter.sendMail({ 
-//     to: "fatihbaydar2004@gmail.com",
-//     subject: "Hello",
-//     text: "Hello. Welcome to our website...",
-//     html: "<h3>Hello. Welcome to our website...</h3><p>How are you?</p>"
-// }, function (error, success) {
-//     success ? console.log("SUCCESS:", success) : console.log("ERROR:", error)
-// }) 
-
 /* ------------------------------------------------------- */
 // Routes:
 
+// Static Files
+// app.use("/url", express.static("./folder"))
+app.use("/upload", express.static("./upload"))
 // routes/index.js:
 app.use('/', require('./src/routes/'))
 
